9ca963d073ef9b32821807799fc772a43f527d08
Oliver Alt <oa@lacheweg21.de>
2023-01-11 16:30:27 +0100

    Error and status handling improved and file import added

--- src/MDD4All.SpecIF.ViewModels/DataImportViewModel.cs
+++ src/MDD4All.SpecIF.ViewModels/DataImportViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using MDD4All.FileAccess.Contracts;
 using MDD4All.SpecIF.Converters;
 using MDD4All.SpecIF.DataProvider.Contracts;
+using MDD4All.SpecIF.ViewModels.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Windows.Input;
 
 namespace MDD4All.SpecIF.ViewModels
@@ -15,21 +18,29 @@ namespace MDD4All.SpecIF.ViewModels
         private IHttpClientFactory _httpClientFactory;
         private ISpecIfMetadataWriter _metadataWriter;
         private ISpecIfDataWriter _dataWriter;
+        private IFileLoader _fileLoader;
 
         public DataImportViewModel(IHttpClientFactory httpClientFactory,
                                    ISpecIfMetadataWriter metadataWriter,
-                                   ISpecIfDataWriter dataWriter)
+                                   ISpecIfDataWriter dataWriter,
+                                   IFileLoader fileLoader)
         {
             _httpClientFactory = httpClientFactory;
             _metadataWriter = metadataWriter;
             _dataWriter = dataWriter;
+            _fileLoader = fileLoader;
+
             InitializeCommands();
         }
 
         private void InitializeCommands()
         {
             ImportSpecIfFromUrlCommand = new RelayCommand(ExecuteImportSpecIfFromUrlCommandAsync);
+            SelectFileCommand = new RelayCommand(ExecuteSelectFileCommand);
+            ImportSpecIfFromFileCommand = new RelayCommand(ExecuteImportSpecIfFromFileCommandAsync);
         }
+
+        
 
         // key: localization string, value: URL
         public Dictionary<string, string> MetadataFiles
@@ -38,15 +49,19 @@ namespace MDD4All.SpecIF.ViewModels
             {
                 Dictionary<string, string> result = new Dictionary<string, string>();
 
-                result.Add("File.SpecIF1.1Release", "https://raw.githubusercontent.com/GfSE/SpecIF-Class-Definitions/7098496f05a0fe071ff46ff172aa58004a9303d9/_Packages/SpecIF-Classes-1_1.specif");
-                result.Add("File.SpecIF1.2Dev", "https://raw.githubusercontent.com/GfSE/SpecIF-Class-Definitions/dev/_Packages/SpecIF-Classes-1_2.specif");
-                result.Add("File.SpecIF1.2DevNonNormative", "https://raw.githubusercontent.com/GfSE/SpecIF-Class-Definitions/dev/_Packages/SpecIF-Classes-1_2_non_normative.specif");
+                result.Add("File.SpecIF1_1Release", "https://raw.githubusercontent.com/GfSE/SpecIF-Class-Definitions/7098496f05a0fe071ff46ff172aa58004a9303d9/_Packages/SpecIF-Classes-1_1.specif");
+                result.Add("File.SpecIF1_2Dev", "https://raw.githubusercontent.com/GfSE/SpecIF-Class-Definitions/dev/_Packages/SpecIF-Classes-1_2.specif");
+                result.Add("File.SpecIF1_2DevNonNormative", "https://raw.githubusercontent.com/GfSE/SpecIF-Class-Definitions/dev/_Packages/SpecIF-Classes-1_2_non_normative.specif");
 
                 return result;
             }
         }
 
         public string MetadataFileURL { get; set; } = "";
+
+        public string LocalFilename { get; set; } = "";
+
+        
 
         public bool OverrideExistingData 
         { 
@@ -82,12 +97,52 @@ namespace MDD4All.SpecIF.ViewModels
             }
         }
 
+        private string _successMessageKey;
+
+        public string SuccessMessageKey
+        {
+            get 
+            {
+                return _successMessageKey; 
+            }
+
+            set
+            {
+                _successMessageKey = value;
+                RaisePropertyChanged("SuccessMessageKey");
+            }
+        }
+
+        private string _errorMessageKey;
+
+        public string ErrorMessageKey
+        {
+            get
+            {
+                return _errorMessageKey;
+            }
+
+            set
+            {
+                _errorMessageKey = value;
+                RaisePropertyChanged("ErrorMessageKey");
+            }
+        }
+
         public Exception Exception { get; set; }
 
+        #region COMMAND_DEFINITIONS
         public ICommand ImportSpecIfFromUrlCommand { get; private set; }
+
+        public ICommand SelectFileCommand { get; private set; }
+
+        public ICommand ImportSpecIfFromFileCommand { get; private set; }
+        #endregion
 
         private async void ExecuteImportSpecIfFromUrlCommandAsync()
         {
+            ClearMessages();
+
             IsImportingData = true;
 
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, MetadataFileURL);
@@ -112,20 +167,114 @@ namespace MDD4All.SpecIF.ViewModels
                             SpecIfConverter specIfConverter = new SpecIfConverter();
                             ProgressMessageKey = "Message.ImportingData";
                             specIfConverter.ConvertAll(specIF, _dataWriter, _metadataWriter, OverrideExistingData);
+
+                            SuccessMessageKey = "Message.ImportSuccess";
+                        }
+                        else
+                        {
+                            ErrorMessageKey = "Error.SpecIfParsing";
                         }
                     }
+                    else
+                    {
+                        ErrorMessageKey = "Error.Download";
+                    }
 
+                }
+                else
+                {
+                    ErrorMessageKey = "Error.Download";
                 }
             }
             catch (Exception exception)
             {
+                
                 Exception = exception;
+                ErrorMessageKey = "Error.ImportError";
             }
             finally
             {
                 IsImportingData = false;
             }
 
+
+        }
+
+        private void ExecuteImportSpecIfFromFileCommandAsync()
+        {
+            ClearMessages();
+
+            IsImportingData = true;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(LocalFilename))
+                {
+
+                    byte[] data = _fileLoader.ReadDataFromFile(LocalFilename);
+
+                    if (data != null)
+                    {
+                        string json = Encoding.UTF8.GetString(data, 0, data.Length);
+
+                        if (json != null)
+                        {
+                            DataModels.SpecIF specIF = JsonConvert.DeserializeObject<DataModels.SpecIF>(json);
+
+                            if (specIF != null)
+                            {
+                                SpecIfConverter specIfConverter = new SpecIfConverter();
+                                ProgressMessageKey = "Message.ImportingData";
+                                specIfConverter.ConvertAll(specIF, _dataWriter, _metadataWriter, OverrideExistingData);
+
+                                SuccessMessageKey = "Message.ImportSuccess";
+                            }
+                            else
+                            {
+                                ErrorMessageKey = "Error.SpecIfParsing";
+                            }
+                        }
+                    }
+                    else
+                    {
+                        ErrorMessageKey = "Error.FileRead";
+                    }
+                }
+            }
+            catch(Exception exception)
+            {
+
+            }
+            finally
+            {
+                IsImportingData = false;
+            }
+        }
+
+        private void ExecuteSelectFileCommand()
+        {
+            if (_fileLoader != null)
+            {
+                string filename;
+                bool dialogResult = _fileLoader.ShowOpenFileDialog(out filename, LocalFilename, "",
+                                                                   "SpecIF (*.specif)|*.specif");
+
+                if (dialogResult)
+                {
+                    if (!string.IsNullOrEmpty(filename))
+                    { 
+                        LocalFilename = filename;
+                    }
+                }
+            }
+        }
+
+        private void ClearMessages()
+        {
+            ProgressMessageKey = "";
+            ErrorMessageKey = "";
+            SuccessMessageKey = "";
+            Exception = null;
 
         }
     }
