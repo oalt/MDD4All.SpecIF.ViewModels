1c3d55ec8e6e7682f16e162e1a0f8d04d8bea00d
Oliver Alt <oa@lacheweg21.de>
2023-01-26 21:57:36 +0100

    View model for stream data subsciption

---
+++ src/MDD4All.SpecIF.ViewModels/DataStreamSubscriberConnectorViewModel.cs
@@ -0,0 +1,69 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using MDD4All.SpecIF.DataProvider.Base.DataStreams;
+using MDD4All.SpecIF.DataProvider.Contracts.DataStreams;
+using System;
+using System.Windows.Input;
+
+namespace MDD4All.SpecIF.ViewModels
+{
+    public class DataStreamSubscriberConnectorViewModel : ViewModelBase
+    {
+        public DataStreamSubscriberConnectorViewModel()
+        {
+            InitializeCommands();
+        }
+
+        private void InitializeCommands()
+        {
+            ConnectCommand = new RelayCommand(ExecuteConnectCommand);
+        }
+
+        private void ExecuteConnectCommand()
+        {
+            if (ConnectAction != null)
+            {
+                ConnectAction();
+            }
+        }
+
+        private bool _isConnecting = false;
+
+        public bool IsConnecting
+        {
+            get
+            {
+                return _isConnecting;
+            }
+
+            set
+            {
+                _isConnecting = value;
+                RaisePropertyChanged("IsConnecting");
+            }
+        }
+
+        private string _statusMessageKey = "";
+
+        public string StatusMessageKey
+        {
+            get
+            {
+                return _statusMessageKey;
+            }
+
+            set
+            {
+                _statusMessageKey = value;
+                RaisePropertyChanged("StatusMessageKey");
+            }
+        }
+
+
+        public Action ConnectAction { get; set; }
+
+        public ISpecIfStreamDataSubscriberProvider SpecIfStreamDataSubscriberProvider { get; set; }
+
+        public ICommand ConnectCommand { get; private set; }
+    }
+}
